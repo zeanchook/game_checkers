25e3dda88c4ed730d5bcc5c0339d13aac5b753c7
zeanchook50 <zhiyanchook@gmail.com>
2024-03-29 21:45:41 +0800

    added several functions(init...event handler)

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
   /*----- constants -----*/
-
-
+  const rowMAX = 10;
+  const colMAX = 10;
+  let checkersColor = 
+  {
+    "1": "black",
+    "-1": "white",
+    "0": "yellow"
+  }
+  
+  
+//   console.log(boardArrVal)
+ 
   /*----- state variables -----*/
+  let boardArrVal = [];
+  const boardRowLen = 8;
+  const boardColLen = 8;
+
+  let prevClickRow ;
+        let prevClickCol;
+
+  
 
 
   /*----- cached elements  -----*/
@@ -13,10 +31,54 @@
 
   /*----- functions -----*/
   init()
+
+  let checker = containerDiv
+  console.log(checker)
+  checker.addEventListener( ("click"),x =>
+    {
+        let checkerClassName = x.target.className
+        let checkerRow = parseInt(x.target.getAttribute("row"))
+        let checkerCol = parseInt(x.target.getAttribute("col"))
+        
+
+        console.log(checkerRow,checkerCol,checkerClassName)
+
+        if(checkerClassName === "whiteChess")
+        {
+            // boardArrVal[checkerRow][checkerCol] = null;
+            
+            for(let i = -1;i < 2;i++)
+            {
+                if (( boardArrVal[checkerRow+1][checkerCol+i] === "null") && ! containerDiv.children[checkerRow+1].children[checkerCol+i].className.includes("blackbckgrd"))
+                {
+                    boardArrVal[checkerRow+1][checkerCol+i] = "possibleMove"
+                }
+                prevClickRow = checkerRow
+                prevClickCol = checkerCol
+                
+            }
+            
+        }
+        else if (checkerClassName === "possibleMove")
+        {
+            
+
+
+            boardArrVal[checkerRow][checkerCol] = boardArrVal[prevClickRow][prevClickCol]
+            boardArrVal[prevClickRow][prevClickCol] = "null"
+            clearPossibleMove(checkerRow);
+        }
+
+        render()
+    })
+
+
   function init()
   {
-    
-    for (let i = 0 ;i < 9;i++)
+    initBoard();
+
+    // create row first
+    for (let i = 0 ;i < boardRowLen;i++)
     {
         let createMainDivRow = document.createElement("div");
         let createMainDivRowClassName = "row";
@@ -24,47 +86,170 @@
         containerDiv.append(createMainDivRow)   
     }
 
+    // the create column 
     for(let row = 0 ; row < containerDiv.childNodes.length;row++)
     {
-        for(let col = 0 ; col < 9 ; col++)
+        for(let col = 0 ; col < boardColLen; col++)
         {
             let createMainDivCol = document.createElement("div");
             let createMainDivColClassName = "col";
-            if(row%2===0)
+            if(row % 2 === 0)
             {   
-                if(col%2===0)
+                if(col % 2 === 0)
                 {
-                    createMainDivColClassName = createMainDivColClassName + " " + "blackbckgrd";
-                    createMainDivCol.setAttribute("class",createMainDivColClassName);
+                    createMainDivCol.setAttribute("class",concatNames(createMainDivColClassName,"blackbckgrd"));
+                    
                 }
-                else{
-                    createMainDivColClassName = createMainDivColClassName + " " + "whitebckgrd";
-                    createMainDivCol.setAttribute("class",createMainDivColClassName);
+                else
+                {
+                    createMainDivCol.setAttribute("class",concatNames(createMainDivColClassName,"whitebckgrd"));
+                             
                 }    
             }
             else
             {
-                if(col%2===1)
+                if(col % 2 !== 0)
                 {
-                    createMainDivColClassName = createMainDivColClassName + " " + "blackbckgrd";
-                    createMainDivCol.setAttribute("class",createMainDivColClassName);
+                    createMainDivCol.setAttribute("class",concatNames(createMainDivColClassName,"blackbckgrd"));    
                 }
-                else{
-                    createMainDivColClassName = createMainDivColClassName + " " + "whitebckgrd";
-                    createMainDivCol.setAttribute("class",createMainDivColClassName);     
+                else
+                {
+                    createMainDivCol.setAttribute("class",concatNames(createMainDivColClassName,"whitebckgrd"));   
+                    
                 }     
             }
+            createCheckers(row,col,createMainDivCol); 
             containerDiv.childNodes[row].append(createMainDivCol);   
-        }
+        }  
     }
-    // render();
+
+    
+    initBoard2();
+    render();
   }
 
   function render()
   {
     renderBoard();
-    renderMessage();
+    // renderMessage();
   }
 
+function createChildNodes (classType)
+{
+    let createBlackChess = document.createElement("div")
+    createBlackChess.setAttribute("class",classType)    
+    return createBlackChess;
+}
 
+function concatNames (variable1,variable2)
+{
+    return (variable1 + " " + variable2)
+}
+
+function createCheckers (currentRowVal,col,parentDiv)
+{
+    let chessClass = parentDiv.className.split(" ")[1]
+    let rowcolAtt = currentRowVal + "-" + col
+    let newChildNode;
+    if((currentRowVal < (boardRowLen/2)-1) && (chessClass !== "blackbckgrd"))
+    { 
+        newChildNode = createChildNodes("whiteChess")
+        newChildNode.setAttribute("row",currentRowVal);
+        newChildNode.setAttribute("col",col);
+        parentDiv.append(newChildNode)
+        boardArrVal[currentRowVal][col] = "whiteChess";
+        return parentDiv;
+    }
+
+    if((currentRowVal >= (boardRowLen/2)+1) && (chessClass !== "blackbckgrd"))
+    {
+        newChildNode = createChildNodes("blackChess")
+        newChildNode.setAttribute("row",currentRowVal);
+        newChildNode.setAttribute("col",col);
+        parentDiv.append(newChildNode)
+        boardArrVal[currentRowVal][col] = "blackChess";
+        return parentDiv;
+    }
+    else
+    { 
+        newChildNode = createChildNodes("none")
+        newChildNode.setAttribute("row",currentRowVal);
+        newChildNode.setAttribute("col",col);
+        parentDiv.append(newChildNode)
+        boardArrVal[currentRowVal][col] = "null";
+        return parentDiv;
+    }
+}
+
+// console.log(boardArrVal[0].length)
+
+function initBoard()
+{
+    boardArrVal.length = boardRowLen;
   
+    for(let i = 0 ; i < boardArrVal.length;i++)
+    {
+        boardArrVal[i] = [];
+        boardArrVal[i].length = boardArrVal.length;
+    }
+}
+
+
+
+function initBoard2()
+{
+    for(let i =0; i < boardArrVal.length;i++)
+    {
+    for(let j = 0; j < boardArrVal[i].length;j++)
+    {
+        if (boardArrVal[i][j] === undefined)
+        {
+            boardArrVal[i][j] = "null";
+        }
+    }
+}
+}
+
+function renderBoard()
+{
+    
+    let containerDivRowLevel = containerDiv.children
+    // console.log(containerDivRowLevel[0].children[3])
+
+    for(let i = 0 ; i < containerDivRowLevel.length ; i++)
+    {
+        // console.log(`row: ${i}`,containerDivRowLevel[i])
+
+        for(let j = 0 ; j < containerDivRowLevel[i].childElementCount; j++)
+        {
+            // console.log(`row: ${i} col: ${j}`,containerDivRowLevel[i].children[j],boardArrVal[i][j])
+
+            if (containerDivRowLevel[i].children[j].hasChildNodes())
+            {
+                // console.log("true")
+                // console.log(`row: ${i} col: ${j}`,containerDivRowLevel[i].children[j],boardArrVal[i][j])
+                containerDivRowLevel[i].children[j].children[0].setAttribute("class",boardArrVal[i][j])
+            }
+            
+        }
+    }
+
+    // console.log(containerDivRowLevel[0].children[3].children[0])
+
+}
+
+function clearPossibleMove(inputRow)
+{
+    boardArrVal
+    for(let i = 0 ; i < boardArrVal[inputRow].length ; i ++)
+    {
+        if(boardArrVal[inputRow][i] === "possibleMove")
+        {
+            boardArrVal[inputRow][i] = "null";
+        }
+    }
+}
+
+
+
+
