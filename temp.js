25a1c9c5fdf39cce90c83940540e5054a240e91a
zeanchook50 <zhiyanchook@gmail.com>
2024-04-01 23:52:14 +0800

    added CSS kings property

--- temp.js
+++ temp.js
@@ -14,10 +14,8 @@
    
   /*----- state variables -----*/
   let boardArrVal = [];
-  const boardRowLen = 6;
-  const boardColLen = 6;
-
-  let removeBefore = 0;
+  const boardRowLen = 10;
+  const boardColLen = 10;
 
   let prevClickRow ;
   let prevClickCol;
@@ -204,23 +202,30 @@ function renderBoard()
 {
     
     let containerDivRowLevel = gamePageDiv.children
+    // console.log(containerDivRowLevel[0].children[3])
 
     for(let i = 0 ; i < containerDivRowLevel.length ; i++)
     {
+        // console.log(`row: ${i}`,containerDivRowLevel[i])
 
         for(let j = 0 ; j < containerDivRowLevel[i].childElementCount; j++)
         {
+            // console.log(`row: ${i} col: ${j}`,containerDivRowLevel[i].children[j],boardArrVal[i][j])
 
             if (containerDivRowLevel[i].children[j].hasChildNodes())
             {
+                // console.log("true")
+                // console.log(`row: ${i} col: ${j}`,containerDivRowLevel[i].children[j],boardArrVal[i][j])
                 containerDivRowLevel[i].children[j].children[0].setAttribute("class",boardArrVal[i][j])
             }
             
         }
     }
+    // console.log(containerDivRowLevel[0].children[3].children[0])
 
 }
 
+// console.log(boardArrVal[1].includes("null"))
 
 function clearPossibleMove()
 {
@@ -251,25 +256,11 @@ function userMoves(event)
 
     if (chkrClsClick === "possibleMove")
     {
-        removeBefore = 0;
         let removeCheckerRow = Math.floor((chkrRowClick + prevClickRow) / 2)
         let removeCheckerCol = Math.floor((chkrColClick + prevClickCol) / 2)
-        
-    
+
         console.log ("removed checker row col: ",removeCheckerRow,removeCheckerCol)
         console.log("previous click: ",prevClickRow,prevClickCol)
-
-       for(let i = 0; i < jumpedOver.length;i++)
-       {
-        if ((removeCheckerRow === jumpedOver[i][0]) && (removeCheckerCol === jumpedOver[i][1]))
-        {
-            console.log("removed row:",removeCheckerRow,removeCheckerCol)
-            console.log("jumped over row:",jumpedOver[i][0],jumpedOver[i][1])
-            delete jumpedOver[i];
-            jumpedOver.pop()
-            removeBefore = 1;
-        }
-       }
 
         boardArrVal[chkrRowClick][chkrColClick] = boardArrVal[prevClickRow][prevClickCol]
 
@@ -281,35 +272,10 @@ function userMoves(event)
 
         boardArrVal[prevClickRow][prevClickCol] = "null"
         
-        clearPossibleMove();
-        jumpedOver = [];
-
-        prevClickRow = chkrRowClick;
-        prevClickCol = chkrColClick;
-
-
-        if((chkrRowClick === boardRowLen - 1) && boardArrVal[chkrRowClick][chkrColClick] === "whiteChess")
-        {
-            console.log("no?")
-            console.log(chkrRowClick,chkrColClick)
-            boardArrVal[chkrRowClick][chkrColClick] += " isKing"
-        }
-
-
-        if (removeBefore)
-        {
-            computePosMoves(turnVar,chkrRowClick,chkrColClick,1) === 0 ? turnVar *= -1 : turnVar
-            console.log("here1")
-            turnVar = turnVar;
-        }
-        else
-        {
-            console.log("here2")
-            turnVar *= -1;
-        }
-        // }
         
-    
+        // boardArrVal[removeCheckerRow][removeCheckerCol] = "null"
+        clearPossibleMove();
+        turnVar *= -1;
     }
         
     // console.log("here",turnVar)
@@ -320,16 +286,16 @@ function userMoves(event)
         prevClickCol = chkrColClick;
         computePosMoves(turnVar,chkrRowClick,chkrColClick,0)
     }
-   
+
     // console.log("rendered")
     render()
 }
 
-function computePosMoves (turns,row,col,kingsMove)
+function computePosMoves (turns,row,col,recurCall)
 {
     let rowForwardBackward;
     turns === 1 ? rowForwardBackward = -1 : rowForwardBackward = 1;
-    let possibleMove = 0;
+
     for(let i = -1 ; i < 2 ; i++)
     {
         let insidePreviousClickRol = row
@@ -338,45 +304,72 @@ function computePosMoves (turns,row,col,kingsMove)
         let currentCol = col + i;
         let currentRow = row + rowForwardBackward;
         currentCol > (boardColLen - 1) ? currentCol = (boardColLen - 1): currentCol < 0 ? currentCol = 0 : currentCol
-
-
-        currentRow > (boardRowLen - 1) ? currentRow = (boardRowLen - 1): currentRow < 0 ? currentRow = 0 : currentRow
         console.log("Current Col: ",currentRow,currentCol)
 
         let directionPosMovesCol = currentCol - prevClickCol;
         let directionPosMovesRow = currentRow - prevClickRow;
 
+        // console.log("Direction: ",col-prevClickCol)
         if (! gamePageDiv.children[currentRow].children[currentCol].className.includes("blackbckgrd"))
         {
             let checkerPosMove = (boardArrVal[currentRow][currentCol])
             let currentVale = "" + checkersProperty[turnVar * -1];
 
-            if (checkerPosMove === "null" && kingsMove === 0)
+            if (checkerPosMove === "null" && recurCall === 0)
             {
                 boardArrVal[currentRow][currentCol] = "possibleMove" 
-                possibleMoveArr.push([currentRow,currentCol])
+                possibleMoveArr.push([currentRow,currentCol])     
             }
 
             else if (checkerPosMove === currentVale)
             {
-                
+                console.log("black chess infron: ", currentRow,currentCol)
+                console.log("Direction: row x col",directionPosMovesRow,directionPosMovesCol)
                 jumpedOver.push([currentRow,currentCol])
+                // console.log(jumpedOver)
+                // console.log("compute direction and future moves: ",currentRow + directionPosMovesRow,currentCol + directionPosMovesCol) ***
+
+                // let computeFutureMovesRol = currentRow + directionPosMovesRow;
+                // let computeFutureMovesCol = currentCol + directionPosMovesCol;
+
                 let computeFutureMovesRol = currentRow + (currentRow - insidePreviousClickRol);
                 let computeFutureMovesCol = currentCol + (currentCol - insidePreviousClickCol);
 
-            
+                // console.log("New compute direction and future moves: ",computeFutureMovesRol,computeFutureMovesCol) ***
+
+                // if (computeFutureMovesRol > (boardRowLen - 1))
+                // {
+                //     computeFutureMovesRol = (boardRowLen - 1)
+                // }
+                // else if (computeFutureMovesRol < 0)
+                // {
+                //     computeFutureMovesRol = 0
+                // }
+
+                // if (computeFutureMovesCol > (boardColLen - 1))
+                // {
+                //     computeFutureMovesCol = (boardColLen - 1)
+                // }
+                // else if (computeFutureMovesCol < 0)
+                // {
+                //     computeFutureMovesCol = 0
+                // }
+
                 if ((computeFutureMovesRol <= (boardRowLen - 1) && computeFutureMovesRol >= 0) && (computeFutureMovesCol <= (boardColLen - 1) && computeFutureMovesCol >= 0))
                 {
+                    // console.log("computeFutureMoves: ",computeFutureMovesRol,computeFutureMovesCol) ***
                     computeDirection (computeFutureMovesRol,computeFutureMovesCol)
-                    possibleMove = 1;
+                    // console.log("prev",prevClickRow,prevClickCol) ***
+                    if(computeFutureMovesRol!== boardRowLen-1 && computeFutureMovesCol !== boardColLen-1)
+                    {
+                        computePosMoves (turnVar,computeFutureMovesRol,computeFutureMovesCol,1)
+                    }
                 }
+                
                 
             }   
         }        
     }
-
-    return possibleMove;
-
 }
 
 function computeDirection (rows,cols)
@@ -386,8 +379,7 @@ function computeDirection (rows,cols)
     {
         boardArrVal[rows][cols] = "possibleMove"
         possibleMoveArr.push([rows,cols])   
+        // possibleMoveArr.push([rows][cols])
     }
 }
-
-
 
